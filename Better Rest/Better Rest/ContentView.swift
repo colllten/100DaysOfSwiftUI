56cca4c0889ccb09345983999fdfeef2517a5595
Colten Glover <coltenglover@yahoo.com>
2023-09-09 14:15:02 -0400

    Added DatePicker

--- Better Rest/Better Rest/ContentView.swift
+++ Better Rest/Better Rest/ContentView.swift
@@ -12,10 +12,15 @@ import CoreData
 
 struct ContentView: View {
     @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
     
     var body: some View {
         NavigationView {
-            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+            VStack {
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                DatePicker("", selection: $wakeUp, in: Date.now...)
+                    .labelsHidden()
+            }
         }
     }
 }
