486dc9566e52140b5f5a9ed831bcd61a3e728de9
Colten Glover <coltenglover@yahoo.com>
2023-09-09 15:19:31 -0400

    Finished layout

--- Better Rest/Better Rest/ContentView.swift
+++ Better Rest/Better Rest/ContentView.swift
@@ -13,14 +13,28 @@ import CoreData
 struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
+    @State private var cupsOfCoffee = 1
     
     var body: some View {
         NavigationView {
             VStack {
-                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-                DatePicker("", selection: $wakeUp, in: Date.now...)
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                DatePicker("Pick a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                     .labelsHidden()
+                    .padding(.bottom)
+                
+                Text("How much would you like to sleep?")
+                    .font(.headline)
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                    .padding(.bottom)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                Stepper("\(cupsOfCoffee) \(cupsOfCoffee == 1 ? " cup" : " cups")", value: $cupsOfCoffee, in: 1...10, step: 1)
+                Spacer()
             }
+            .padding(.horizontal)
         }
     }
 }
