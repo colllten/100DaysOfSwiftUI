bf99bbab410db31e9fccddb3f6a1332b863536b3
Colten Glover <coltenglover@yahoo.com>
2023-09-19 18:11:33 -0400

    Initial commit

---
+++ FirebaseBootcamp/FirebaseBootcamp/FirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseBootcampApp.swift
+//  FirebaseBootcamp
+//
+//  Created by Colten Glover on 9/19/23.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
