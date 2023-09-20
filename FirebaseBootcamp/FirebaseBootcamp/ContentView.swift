07079f46cbf27e163cf11f87df728f353cc8ee4a
Colten Glover <coltenglover@yahoo.com>
2023-09-20 18:23:35 -0400

    Minor changes to add alert

--- FirebaseBootcamp/FirebaseBootcamp/ContentView.swift
+++ FirebaseBootcamp/FirebaseBootcamp/ContentView.swift
@@ -6,16 +6,49 @@
 //
 
 import SwiftUI
+import FirebaseAuth
 
 struct ContentView: View {
+    @State private var email = ""
+    @State private var password = ""
+    @State private var confirmPassword = ""
+    
+    @State private var errorShowing = false
+    @State private var errorMessage = ""
+    
+    @State var matching = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Section("Email") {
+                TextField("", text: $email)
+            }
+            .keyboardType(.emailAddress)
+            .autocorrectionDisabled()
+            .textInputAutocapitalization(.never)
+            
+            Section("Password") {
+                SecureField("Password", text: $password)
+                SecureField("Confirm Password", text: $confirmPassword)
+            }
+            
+            Button("Submit") {
+                Auth.auth().createUser(withEmail: email, password: password) { result, error in
+                    if let error {
+                        print("showing error")
+                        errorShowing = true
+                        errorMessage = error.localizedDescription
+                        return
+                    } else {
+                        print("All good!")
+                    }
+                }
+            }
         }
-        .padding()
+        .alert("Error creating user", isPresented: $errorShowing) {
+            Button("OK") {}
+        } message: {
+            Text(errorMessage)
+        }
     }
 }
 
--- FirebaseBootcamp/FirebaseBootcamp/FirebaseBootcampApp.swift
+++ FirebaseBootcamp/FirebaseBootcamp/FirebaseBootcampApp.swift
@@ -7,16 +7,22 @@
 
 import SwiftUI
 import Firebase
+import FirebaseCore
 
 @main
 struct FirebaseBootcampApp: App {
-    init() {
-        FirebaseApp.configure()
-    }
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
     }
 }
