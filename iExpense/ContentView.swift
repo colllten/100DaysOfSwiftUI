d9208ba17b35e53408806589e2cedf19163dba5c
Colten Glover <coltenglover@yahoo.com>
2023-09-25 21:41:33 -0400

    Can encode object into User Defaults

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,14 +7,22 @@
 
 import SwiftUI
 
+struct User: Codable {
+    let firstName: String
+    let lastName: String
+}
+
 struct ContentView: View {
-    @AppStorage("tapCount") private var tapCount = 0
+    @State private var user = User(firstName: "Colten", lastName: "Glover")
     
     var body: some View {
         NavigationView {
             VStack {
-                Button("Tap Count: \(tapCount)") {
-                    tapCount += 1
+                Button("Save User") {
+                    let encoder = JSONEncoder()
+                    if let data = try? encoder.encode(user) {
+                        UserDefaults.standard.set(data, forKey: "UserData")
+                    }
                 }
             }
         }
