fa73b1004d997575f3f29e593d3d147d273145b3
Colten Glover <coltenglover@yahoo.com>
2023-09-30 14:34:59 -0400

    Defined an expense

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,23 +7,12 @@
 
 import SwiftUI
 
-struct User: Codable {
-    let firstName: String
-    let lastName: String
-}
-
 struct ContentView: View {
-    @State private var user = User(firstName: "Colten", lastName: "Glover")
     
     var body: some View {
         NavigationView {
             VStack {
-                Button("Save User") {
-                    let encoder = JSONEncoder()
-                    if let data = try? encoder.encode(user) {
-                        UserDefaults.standard.set(data, forKey: "UserData")
-                    }
-                }
+                
             }
         }
     }
---
+++ iExpense/iExpense/Model/ExpenseItem.swift
@@ -0,0 +1,14 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Colten Glover on 9/30/23.
+//
+
+import Foundation
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
