da9a2d53772d957b1f106d29c1ff79e0a8891751
Colten Glover <coltenglover@yahoo.com>
2023-10-09 12:45:24 -0400

    Added data to present to screen

--- Moonshot/Moonshot/Bundle-Decodable.swift
+++ Moonshot/Moonshot/Bundle-Decodable.swift
@@ -16,8 +16,11 @@ extension Bundle {
         guard let data = try? Data(contentsOf: url) else {
             fatalError("Failed to load \(file) in the bundle")
         }
-        
-        guard let loaded = try? JSONDecoder().decode(T.self, from: data) else {
+        let decoder = JSONDecoder()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "y-MM-dd"
+        decoder.dateDecodingStrategy = .formatted(formatter)
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
             fatalError("Failed to decode \(file) in the bundle")
         }
         
