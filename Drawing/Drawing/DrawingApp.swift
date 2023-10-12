fb30a84befb8183f97e5260a61c84566c7f19fd0
Colten Glover <coltenglover@yahoo.com>
2023-10-11 21:30:04 -0400

    Finished Moonshot

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Colten Glover on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
