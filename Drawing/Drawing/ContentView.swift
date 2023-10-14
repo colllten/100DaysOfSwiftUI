82ee4776c69778558611b7c6491853cc29bdd7b9
Colten Glover <coltenglover@yahoo.com>
2023-10-14 18:38:53 -0400

    Created blur and saturation effects

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,47 +7,16 @@
 
 import SwiftUI
 
-struct Triangle: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
-
-        return path
-    }
-}
-
-struct Arc: InsettableShape {
-    var startAngle: Angle
-    var endAngle: Angle
-    var clockwise: Bool
-    
-    var insetAmount = 0.0
-
-    func path(in rect: CGRect) -> Path {
-        let rotationAdjustment = Angle.degrees(90)
-        let modifiedStart = startAngle - rotationAdjustment
-        let modifiedEnd = endAngle - rotationAdjustment
-
-        var path = Path()
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
-
-        return path
-    }
-    
-    func inset(by amount: CGFloat) -> some InsettableShape {
-        var arc = self
-        arc.insetAmount += amount
-        return arc
-    }
-}
-
 struct ContentView: View {
+    @State private var amount = 0.0
     var body: some View {
-        Arc(startAngle: .degrees(0), endAngle: .degrees(120), clockwise: true)
+        Image("armstrong")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 200, height: 200)
+            .saturation(amount)
+            .blur(radius: (1 - amount) * 90)
+        Slider(value: $amount)
     }
 }
 
