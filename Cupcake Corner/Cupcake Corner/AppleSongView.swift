e022c440acdb663688cfed1a1889835b3335e951
Colten Glover <coltenglover@yahoo.com>
2023-10-26 13:17:02 -0400

    Can disable buttons based on conditions

--- Cupcake Corner/Cupcake Corner/AppleSongView.swift
+++ Cupcake Corner/Cupcake Corner/AppleSongView.swift
@@ -7,54 +7,28 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var message: String
-}
-
 struct AppleSongView: View {
-    @State private var results = [Result]()
-    @State private var result = Result(message: "")
+    @State var username = ""
+    @State var email = ""
     
-    var body: some View {
-        VStack {
-            AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { image in
-                image
-                    .resizable()
-                    .scaledToFit()
-                
-            } placeholder: {
-                ProgressView()
-            }
-            .frame(width: 200, height: 200)
-        }
-        .task {
-            //await loadData()
-        }
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
     }
     
-    func loadData() async {
-        guard let url = URL(string: "https://techy-api.vercel.app/api/json") else {
-            print("Invalid URL")
-            return
-        }
-        
-        do {
-            print("in do")
-            let (data, _) = try await URLSession.shared.data(from: url)
-            if let decodedResponse = try? JSONDecoder().decode(Result.self, from: data) {
-                print("decoded")
-                result.message = decodedResponse.message
-                print("Loaded!")
+    var body: some View {
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
             }
-        } catch {
-            print("Invalid data")
+            
+            Section {
+                Button("Create account") {
+                    //code
+                }
+            }
+            .disabled(disableForm)
         }
-        
-        
     }
 }
 
